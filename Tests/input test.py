84d1654e9eadf21caa4946e83548fca8f0987809
Abdul Musthakin <musa232016@gmail.com>
2022-10-18 21:19:39 +0100

    modified:   Tests/input test.py

--- Tests/input test.py
+++ Tests/input test.py
@@ -4,4 +4,11 @@ if x == "true":
     x = True
 else:
     x = False
-print("It is", str(x).lower(), "that you like Turtles") 
+print("It is", str(x).lower(), "that you like Turtles")
+
+dict = {
+"Bob": 5,
+"Jim" : 3,
+"Tim": 8
+}
+print(sorted(dict.items(), key = lambda x:x[1]))
