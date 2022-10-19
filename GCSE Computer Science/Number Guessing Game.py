a8477c0f9398e541b38f94449c7a89a57e703493
Abdul Musthakin <musa232016@gmail.com>
2022-10-19 21:48:30 +0100

    modified:   GCSE Computer Science/Number Guessing Game.py Major, possibly final commit to this

--- GCSE Computer Science/Number Guessing Game.py
+++ GCSE Computer Science/Number Guessing Game.py
@@ -6,19 +6,34 @@ def intro():
     print(f"Hello, welcome to my number guessing game.\nYou will guess a number and be told if it's the correct one.\nThis is a very skill-reliant game so try your best and have fun!")
 leaderboard = {
 "Bert" : 2,
-"Tim" : 4,
-"Bartholamew" : 5,
-"Bob" : 99,
+"Jeff": 4,
+"Tim" : 8,
+"Einstein" : 20,
+"Bartholamew" : 11,
+"Ibrahim" : 25,
+"Zhang Wei" : 99,
 } 
 
-lives = 5
+retry = 5
+startlives = 5
+score = 0
 guess = 0
-intro()
-
-x = 0
 y = "None"
 while y.lower() not in difficultylist:
-    y = input(f"Choose a difficulty(Easy, Medium, Hard, Impossible):")
+        y = input(f"Choose a difficulty(Easy, Medium, Hard, Impossible):")
+        if y.lower() == "easy":
+            x = random.randint(1, 10)
+        elif y.lower() == "medium":
+            x = random.randint(1, 20)
+        elif y.lower() == "hard":
+            x = random.randint(1, 50)
+        elif y.lower() == "impossible":
+            x = random.randint(1, 1000)
+
+intro()
+x = 0
+
+while retry != 0 :
     if y.lower() == "easy":
         x = random.randint(1, 10)
     elif y.lower() == "medium":
@@ -27,38 +42,44 @@ while y.lower() not in difficultylist:
         x = random.randint(1, 50)
     elif y.lower() == "impossible":
         x = random.randint(1, 1000)
-
-
-guess = int(input(f"Guess the Number:"))
-if guess != x:
-    lives -=1
-if guess > x:
-    print(f"Your guess was too high")
-elif guess < x:
-    print(f"Your guess was too low")
-else: pass
-if guess != x:
-    print (f"You have {lives} lives left.")
-while guess != x and lives > 0:
-    guess = int(input(f"Try again:"))
+    lives = startlives
+    guess = int(input(f"Guess the Number:"))
     if guess != x:
-        lives -= 1
+        lives -=1
     if guess > x:
         print(f"Your guess was too high")
     elif guess < x:
         print(f"Your guess was too low")
     else: pass
-    print(f"You have {lives} lives left.") if lives > 0 and guess != x else 0
-sleep(1)
+    if guess != x:
+        print (f"You have {lives} lives left.")
+    while guess != x and lives > 0:
+        guess = int(input(f"Try again:"))
+        if guess != x:
+            lives -= 1
+        if guess > x:
+            print(f"Your guess was too high")
+        elif guess < x:
+            print(f"Your guess was too low")
+        else: pass
+        print(f"You have {lives} lives left.") if lives > 0 and guess != x else 0
+    sleep(1)
+    if lives > 0:
+        print(f"You guessed the right number! And you only needed {5 - lives} tries")
+    else: print(f"You ran out of tries. The answer was {x}")
+    sleep(1)
+    score += lives
+    retry -= 1
+    if retry != 0:
+        a = input("Press enter to retry and accumulate more points(or enter 'no' to end):")
+        if a.lower() == "no":
+            break
+    sleep(1)
 
-if lives > 0:
-    print(f"You guessed the right number! And you only needed {5 - lives} tries")
-else: print(f"You ran out of tries. The answer was {x}")
-sleep(1)
 print("This game has been completed by other people as well,\nYou will be showed the leaderboard with your score added in a second")
 sleep(1)
 name = input("Please enter your name:")
-leaderboard[f"{name}"] = lives
+leaderboard[f"{name}"] = score
 
 sortedleaderboard = dict(sorted(leaderboard.items(), key = lambda x:x[1])) 
 """
@@ -68,4 +89,4 @@ the items() method is used to extract the items from the dictionary.
 The lambda function has x[1] because that it is the index of the value part of the items.
 """
 sleep(2)
-print(sortedleaderboard)
+print(f"Leaderboard:\n{sortedleaderboard}")
