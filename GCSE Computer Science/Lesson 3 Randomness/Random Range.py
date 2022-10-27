a201c0f4566266236d017c19907646c8b9bb5ae2
Abdul Musthakin <musa232016@gmail.com>
2022-10-27 22:34:04 +0100

    modified:   GCSE Computer Science/Lesson 3 Randomness/Random Range.py

--- GCSE Computer Science/Lesson 3 Randomness/Random Range.py
+++ GCSE Computer Science/Lesson 3 Randomness/Random Range.py
@@ -1,5 +1,4 @@
 from random import randrange
 
 while True:
-    my_number = randrange(1, 10, 2) # range between 1 and 10, numbers have increment of 2(i.e. skipping every other integer)
-    print(my_number)
+    my_number = randrange(1, 10, 2) # range between 1 and 10, numbers have increment of 2(i.e. counts up in 2
