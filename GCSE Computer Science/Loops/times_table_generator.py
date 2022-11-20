f6862a66f5991ab50ff2bf292617773666cd6f4a
Abdul Musthakin <musa232016@gmail.com>
2022-11-20 00:58:03 +0000

    modified:   GCSE Computer Science/Loops/times_table_generator.py 	modified:   Tests/__pycache__/numpy_test.cpython-310-pytest-7.1.3.pyc 	modified:   Tests/numpy_test.py

---
+++ GCSE Computer Science/Loops/times_table_generator.py
@@ -0,0 +1,29 @@
+def times_table_generator():
+    """
+    Asks user for a number to find the product of and iterates it from 1 to the max value given
+    """
+    is_valid = False
+    times_table = 0
+    while is_valid is False:
+        try:
+            times_table = int(input("What number do you want the times table of? \n"))
+            if times_table <= 0:
+                raise ValueError
+            is_valid = True
+        except ValueError:
+            print("You must enter a number more than 0")
+    is_valid = False
+    max_value = 0
+    while is_valid is False:
+        try:
+            max_value = int(input("What do you want to go up to(number * upper_bound)? \n")) + 1
+            if max_value <= 1:
+                raise ValueError
+            is_valid = True
+        except ValueError:
+            print("You must enter a number above 1")
+    print("Here is the", times_table, "times table:")
+    for i in range(1, max_value):
+        answer = i * times_table
+        print(i, "times", times_table, "is", answer)
+times_table_generator()
--- Tests/numpy_test.py
+++ Tests/numpy_test.py
@@ -1,6 +1,1 @@
-import sympy as sym
-
-x = sym.symbols('x')
-p = (x**2 + 8*x + 16)
-q = sym.solve(p)
-print(q)
+import matplotlib.pyplot as plt
