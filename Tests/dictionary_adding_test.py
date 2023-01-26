75bb1d8fd552f92522486d5bd01cf403f5e09c16
Abdul Musthakin <musa232016@gmail.com>
2023-01-26 23:31:26 +0000

    new file:   Tests/dictionary_adding_test.py

---
+++ Tests/dictionary_adding_test.py
@@ -0,0 +1,7 @@
+total_ice_creams = {}
+user_input = input("Enter the name of the ice cream: ")
+# write code to add the inputed ice cream to the dictionary. Then, add 100 to the value of the ice cream. Let the user add 100 as much as they want.
+
+total_ice_creams[user_input] = 100
+total_ice_creams[user_input] += 100
+print(total_ice_creams)
