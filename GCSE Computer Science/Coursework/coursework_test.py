13bbbe6aaca7c6c85de4dd04b8e3c6390b7afb4a
Abdul Musthakin <musa232016@gmail.com>
2023-03-05 16:57:54 +0000

    added a way to automate testing for coursework

--- GCSE Computer Science/Coursework/coursework_test.py
+++ GCSE Computer Science/Coursework/coursework_test.py
@@ -3,12 +3,21 @@ from unittest.mock import patch
 import python_coursework_home as pch
 
 
-class CourseworkTest(unittest.TestCase):
+class TestCoursework(unittest.TestCase):
     @patch("builtins.input")
     def test_chosen_message(self, mocked_input):
         mocked_input.side_effect = ["y", "Bob", "Test"]
 
         self.assertEqual(pch.choose_personalised_message(), "Bob. Test")
+
+    @patch("builtins.input")
+    def test_choose_tin_contents(self, mocked_input):
+        mocked_input.side_effect = ["", "1", 500, "y", "2", 400, "y", "3", 100, "y"]
+
+        self.assertEqual(
+            pch.choose_tin_contents(),
+            {"Caramel Twist": 500, "Orange Crush": 400, "Chocolate Bar": 100},
+        )
 
 
 if __name__ == "__main__":
