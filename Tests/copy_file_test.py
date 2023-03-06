33848594f28e52a295ee40b37ee3665edb68fbce
Abdul Musthakin <musa232016@gmail.com>
2023-03-06 00:18:35 +0000

    .

--- Tests/copy_file_test.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-
-for i in range(10):
-    shutil.copy2(
-        r"C:\Users\Owner\Documents\Python\Python-repository\Tests\copy.txt",
-        rf"C:\Users\Owner\Desktop\copy{i}.txt",
-    )
