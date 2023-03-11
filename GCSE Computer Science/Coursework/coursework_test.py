be3b7b8ab6b13b8ee8f2157352917f3fc6cd01ff
Abdul Musthakin <musa232016@gmail.com>
2023-03-11 11:45:49 +0000

    .

--- GCSE Computer Science/Coursework/coursework_test.py
+++ GCSE Computer Science/Coursework/coursework_test.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import patch
 import python_coursework_home as pch
+import sys
+import os
 
 
 class TestCoursework(unittest.TestCase):
@@ -20,5 +22,38 @@ class TestCoursework(unittest.TestCase):
         )
 
 
+class SaveTests:
+    def __init__(self):
+        self.location = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+        self.log_file = os.path.join(self.location, "test_log.txt")
+        self.results = os.path.join(self.location, "test_results.txt")
+
+    def log_tests(self):
+
+        with open(self.log_file, "w") as f:
+            suit = unittest.TestLoader().loadTestsFromTestCase(TestCoursework)
+            unittest.TextTestRunner(stream=f, verbosity=2, buffer=True).run(suit)
+
+    def export_test_results(self):
+
+        with open(self.results, "r+") as f:
+            lines = f.readlines()
+            lines = [line.strip() if line.strip() != "" else line for line in lines]
+            print(lines)
+            last_line = lines[-1]
+            test_num = str(int(last_line.split(" ")[1][:-1]) + 1)
+
+            with open(self.log_file, "r") as g:
+                f.write("~" * 50 + "\n")
+                f.write(g.read())
+                f.write("~" * 50 + "\n\n")
+                f.write("Test " + test_num + ")\n")
+
+
 if __name__ == "__main__":
-    unittest.main()
+
+    st = SaveTests()
+    st.log_tests()
+    st.export_test_results()
